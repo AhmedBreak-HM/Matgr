e9a7164f7cd3730e5e179bac42e7f4a5f55e3fb4
ahmed break <ahmed_break8@yahoo.com>
2022-10-29 23:16:53 +0300

    Add DbContext and Application User

---
+++ Matgr.IdentityServer/Data/IdentityServerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Matgr.IdentityServer.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Matgr.IdentityServer.Data
+{
+    public class IdentityServerDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityServerDbContext(DbContextOptions<IdentityDbContext> options): base(options)
+        {
+
+        }
+
+
+    }
+}
---
+++ Matgr.IdentityServer/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Matgr.IdentityServer.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FristName { set; get; }
+
+        public string LastName { set; get; }
+    }
+}
